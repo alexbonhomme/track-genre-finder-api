4a2356033584277590bd4f4b11bd2473089426ca
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2021-01-21 19:45:46 +0100

    feat(itunes): impl itunes search

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItunesModule } from './itunes/itunes.module';
 
 @Module({
-  imports: [],
+  imports: [ItunesModule],
   controllers: [AppController],
   providers: [AppService],
 })
